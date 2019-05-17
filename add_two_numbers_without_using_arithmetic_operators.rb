ade31c1439da177bf07b49e65c8104ffb12cdc51
Nishi Kant Sharma <nishiit24@gmail.com>
2019-05-17 13:13:13 +0530

    Committed to add a new file to add two numbers without using arithmetic operators

---
+++ add_two_numbers_without_using_arithmetic_operators.rb
@@ -0,0 +1,20 @@
+=begin
+Add two numbers without using arithmetic operators
+Examples:
+    Input : a = 10
+    Input : b = 20
+    Output : 30    
+=end
+
+a = 10
+b = 20
+
+while b > 0
+
+	a += 1    #post increment a++
+	
+	b -= 1    #post decrement --b
+
+end
+
+puts a
